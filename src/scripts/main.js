2a37b99e44b8979c75002f10c70243fabd641a8f
wisejackal <spam.wabuo.googlemail.com>
2025-12-18 00:18:30 +0800

    add  astro preloader, make sure the site works wit no script, move blog slucg in to theme folder

--- src/scripts/main.js
+++
@@ -1,4 +0,0 @@
-// src/scripts/main.js
-// DEPRECATED: This file has been refactored into individual Astro components 
-// (Hero.astro, Navigation.astro, BaseLayout.astro) for better performance and modularity.
-// Please do not use this file.
